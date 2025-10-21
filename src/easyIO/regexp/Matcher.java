edf98008389b9bd579a90bd8f31bc046f652bfb4
Andrew Myers <andru@cs.cornell.edu>
2025-10-20 21:42:08 -0400

    Clean out some ill-considered capture code, rename Hopeless.

--- src/easyIO/regexp/Matcher.java
+++ src/easyIO/regexp/Matcher.java
@@ -12,26 +12,13 @@ import static easyIO.StdIO.println;
 public class Matcher {
     private static final boolean DEBUG = false;
     private final ArrayList<String> captures = new ArrayList<>();
-    private StringBuilder currentCaptureBuilder = null;
-    private Capture currentCapture = null;
     private State state;
     private RegExp expr;
-    record Hopeless(int index, RegExp r) {}
-    private Set<Hopeless> hopelessStates = null;
+    record IndexedState(int index, RegExp r) {}
+    private Set<IndexedState> hopelessStates = null;
     public Matcher(RegExp r) {
         expr = r;
         state = new State(r);
-    }
-
-    public void startCapture(Capture c) {
-        if (currentCapture == c) return;
-        currentCapture = c;
-        currentCaptureBuilder = new StringBuilder();
-    }
-    public void finishCapture() {
-        captures.add(currentCaptureBuilder.toString());
-        currentCapture = null;
-        currentCaptureBuilder = null;
     }
 
     public record State (
@@ -45,8 +32,7 @@ public class Matcher {
         return match(new BacktrackScanner(new BacktrackScanner.ReaderSource(new StringReader(s), s)));
     }
     private void reportState(int pos, State state, int ch) {
-        print(pos + ". State is now RE=" + state.re + "  captures=" + captures + " current = " +
-                (currentCapture == null ? "none" : currentCaptureBuilder.toString()));
+        print(pos + ". State is now RE=" + state.re);
         if (ch != 0) println(", input character = " + ((char) ch));
         else println();
     }
@@ -60,7 +46,6 @@ public class Matcher {
                 break;
             }
             if (DEBUG) reportState(s.inputPosition(), state, ch);
-            if (currentCapture != null) currentCaptureBuilder.appendCodePoint(ch);
             state = state.re.scan(ch, this);
             if (state.re.isVoid()) {
                 throw FailedMatch.exception;
@@ -80,10 +65,12 @@ public class Matcher {
      *  are returned. If no occurrence is found, the scanner goes to the end of the input.
      */
     public List<String> search(BacktrackScanner s) throws FailedMatch {
+        // This implementation avoids backtracking by memoizing hopeless states, so
+        // it avoids the typical quadratic worst-case complexity of maximal munch tokenization.
         boolean success = false;
         state = new State(expr);
         if (hopelessStates == null) hopelessStates = new HashSet<>();
-        Map<Integer,Hopeless> seenStates = new HashMap<>();
+        Map<Integer, IndexedState> seenStates = new HashMap<>(); // states seen along the way
         s.mark(); // start mark
         int end = s.inputPosition();
         while (s.hasNext()) {
@@ -99,7 +86,7 @@ public class Matcher {
                 if (DEBUG) reportState(pos, state, ch);
                 state = re.scan(ch, this);
 
-                seenStates.put(pos, new Hopeless(pos, state.re));
+                seenStates.put(pos, new IndexedState(pos, state.re));
             } catch (EOF exc) {
                 break;
             }
@@ -107,14 +94,14 @@ public class Matcher {
                 success = true;
                 s.mark(); // end mark, so we can roll back to this position
                 end = s.inputPosition();
-            } else if (state.re.isVoid() || hopelessStates.contains(new Hopeless(s.inputPosition(), state.re))) {
-                if (DEBUG && hopelessStates.contains(new Hopeless(s.inputPosition(), state.re))) {
+            } else if (state.re.isVoid() || hopelessStates.contains(new IndexedState(s.inputPosition(), state.re))) {
+                if (DEBUG && hopelessStates.contains(new IndexedState(s.inputPosition(), state.re))) {
                     println("Abandoning early because state is hopeless");
                 }
                 if (success) {
                     int pos = s.inputPosition();
                     for (int i = end; i < pos; i++) {
-                        Hopeless h = seenStates.get(i);
+                        IndexedState h = seenStates.get(i);
                         assert h != null;
                         hopelessStates.add(h);
                     }
@@ -129,13 +116,12 @@ public class Matcher {
                 state = new State(expr);
             }
         }
-        if (success) {
+        if (success) { // XXX this repeats code from above.
             int pos = s.inputPosition();
             for (int i = end; i < pos; i++) {
-                Hopeless h = seenStates.get(i);
+                IndexedState h = seenStates.get(i);
                 assert h != null;
                 hopelessStates.add(h);
-                println("Hopeless state: " + h);
             }
             // roll back to previous end mark
             s.abort();
